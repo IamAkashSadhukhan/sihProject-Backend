c17211e98fddeb9bd1d5b3dcbba20560763d41fb
Akash Sadhukhan <akashsadhukhan369@gmail.com>
2025-09-27 21:39:41 +0530

    conflict fixed

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,7 +2,7 @@ import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import userModel from "../models/userModel.js";
 import transporter from "../config/nodemailer.js";
-import { EMAIL_VERIFY_TEMPLATE,PASSWORD_RESET_TEMPLATE } from "../config/EmailTemplates.js";
+import { EMAIL_VERIFY_TEMPLATE,PASSWORD_RESET_TEMPLATE } from "../config/emailTemplates.js";
 export const register = async (req, res) => {
   const { name, email, password } = req.body;
   if (!name || !email || !password) {
